0aa3a6e80ad3472dcba1b4db0b9e19354ef50692
bvultrex <b.vultrex@gmail.com>
2026-09-17 22:33:17 +0200

    Enable v0.3.13 controller-oriented tablet patch

--- scripts/apply_patches.sh
+++ scripts/apply_patches.sh
@@ -31,6 +31,7 @@ run_fragmented_patch patch_v039
 run_fragmented_patch patch_v040
 run_fragmented_patch patch_v041
 run_fragmented_patch patch_v042
+run_fragmented_patch patch_v043
 
 echo '--- Modular patch verification ---'
 SRC="$SRC_DIR/XrPassthroughOcclusionGl.cpp"
@@ -51,6 +52,7 @@ grep -n 'kMaxTriangles = 2000000' "$SRC"
 grep -n 'kMaxTextureGpuBytes = 160ULL' "$SRC"
 grep -n 'safe boot ignored persisted GLB' "$SRC"
 grep -n 'peak-memory guard' "$SRC"
+grep -n 'v0.3.13 controller-oriented tablet placement' "$SRC"
 grep -n 'returnToViewer' meta-openxr-sdk/Samples/XrSamples/XrPassthroughOcclusion/Projects/Android/questmr/java/com/bvultrex/quest3mrmodelviewer/QuestMrPickerActivity.java
 grep -n 'QUESTMR_V039_VISIBLE_PICKER' meta-openxr-sdk/Samples/XrSamples/XrPassthroughOcclusion/Projects/Android/questmr/java/com/bvultrex/quest3mrmodelviewer/QuestMrPickerActivity.java
 grep -n 'QUESTMR_V041_CLEAR_STALE_IMPORT' meta-openxr-sdk/Samples/XrSamples/XrPassthroughOcclusion/Projects/Android/questmr/java/com/bvultrex/quest3mrmodelviewer/QuestMrPickerActivity.java
