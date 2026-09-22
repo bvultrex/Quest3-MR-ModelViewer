3eee5adb8277ac8feae74b0b9c2b139065591d37
bvultrex <b.vultrex@gmail.com>
2026-09-22 23:53:38 +0200

    Apply and verify tablet pointer ray fix

--- scripts/apply_patches.sh
+++ scripts/apply_patches.sh
@@ -39,6 +39,7 @@ run_fragmented_patch patch_v047
 run_fragmented_patch patch_v048
 run_fragmented_patch patch_v049
 run_fragmented_patch patch_v050
+run_fragmented_patch patch_v051
 
 echo '--- Modular patch verification ---'
 SRC="$SRC_DIR/XrPassthroughOcclusionGl.cpp"
@@ -79,6 +80,8 @@ grep -n 'std::vector<QuestMrImportedMaterial> materials' "$SRC"
 grep -n 'QuestMR: v1.1 GLB primitives=' "$SRC"
 grep -n 'GL_SRGB8_ALPHA8' "$SRC"
 grep -n 'GL_TEXTURE_MAX_ANISOTROPY_EXT' "$SRC"
+grep -n 'QuestMR v1.1.1: true controller ray -> tablet plane cursor' "$SRC"
+grep -n 'rayDistance > 3.0f' "$SRC"
 grep -n 'peak-memory guard' "$SRC"
 grep -n 'returnToViewer' meta-openxr-sdk/Samples/XrSamples/XrPassthroughOcclusion/Projects/Android/questmr/java/com/bvultrex/quest3mrmodelviewer/QuestMrPickerActivity.java
 grep -n 'QUESTMR_V039_VISIBLE_PICKER' meta-openxr-sdk/Samples/XrSamples/XrPassthroughOcclusion/Projects/Android/questmr/java/com/bvultrex/quest3mrmodelviewer/QuestMrPickerActivity.java
